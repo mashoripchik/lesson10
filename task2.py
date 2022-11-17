2dc2126cf3d9d26328ab982b0828cc4f87d72e0d
Tatyana <mashoripchik@gmail.com>
2022-11-17 20:21:49 +0300

    lesson 10

---
+++ task2.py
@@ -0,0 +1,13 @@
+from random import random, randint
+a1 = []
+a2 = []
+for i in range(10):
+    a1.append(randint(0, 5))
+    a2.append(randint(-5, 0))
+print(a1)
+print(a2)
+a1 = tuple(a1)
+a2 = tuple(a2)
+c = a1 + a2
+print((c), c.count(0))
+
